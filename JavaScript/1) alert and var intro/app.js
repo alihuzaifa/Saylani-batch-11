23c5a1f0ce2190a338365c22fbb5175442fdb84a
Shehzad Iqbal <38328235+skahmerali@users.noreply.github.com>
2024-02-23 23:04:34 +0500

    javascript introduction class

---
+++ JavaScript/1) alert and var intro/app.js
@@ -0,0 +1,22 @@
+var greetmessage = "Assalamu Alaikum\n This is Huzaifa An it Student who is currently working as a developer";
+
+// alert(greetmessage)
+// alert(greetmessage)
+// alert(greetmessage)
+// alert(greetmessage)
+
+var mathematics = 1247247242424;
+// will create a box or container
+// evry variable must have a name
+var number1 = 100;
+var number2 = 100;
+var addmessage = "Here is your sum "
+var submessage = "Here is your sub "
+var mulmessage = "Here is your multiplication "
+var divmessage = "Here is your division "
+// alert(mulmessage + (number1 * number2))
+
+var obtainmarks = 40;
+var totalmarks = 50;
+var percentage = (obtainmarks / totalmarks * 100) + "%"
+alert(percentage);
