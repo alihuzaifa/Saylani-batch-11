afcc2f8cf3994fb504d11336ab475e9eb9d70184
skahmerali <38328235+skahmerali@users.noreply.github.com>
2024-03-04 23:08:02 +0500

    functions and prompt

---
+++ JavaScript/4) Functions/app.js
@@ -0,0 +1,36 @@
+// var number1 = +prompt('Enter Your First Number');
+// console.log(number1 + 200)
+
+// var firstName = prompt('Enter Your First Name');
+// var lastName = prompt('Enter Your Last Name');
+// var fullName = firstName + " " + lastName;  // Concatenation
+// console.log(fullName)
+
+
+// var userAge = +prompt('Enter Your Age', 10)
+// console.log(userAge + 9)
+
+
+
+
+
+// // Parameter
+// function sum(num1, num2, num3) {
+//     console.log(num1, num2, num3)
+// }
+
+// sum(10, 20, 30) // Arguement
+
+
+// function fullName(name1, name2) {
+//     alert("Hello " + name1 + " " + name2)
+// }
+
+// fullName("Bashir", "Ahmed")
+
+
+function isEven() {
+    var userNumber = +prompt('Enter a Number?');
+    console.log(userNumber % 2)
+}
+isEven()
