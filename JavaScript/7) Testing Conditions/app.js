8efbd4f4f4de55c0975d9550784cd8824094ea9a
skahmerali <38328235+skahmerali@users.noreply.github.com>
2024-03-13 07:54:56 +0500

    Testing Condition by && or || operator in javascript

---
+++ JavaScript/7) Testing Conditions/app.js
@@ -0,0 +1,52 @@
+// var try1 = prompt('Choose your dice');
+// var try2 = prompt('Choose your dice');
+// var try3 = prompt('Choose your dice');
+
+// try1 and try2 and try3 === "o" 
+// === !== > < >= <= 
+
+// && === aur || === "ya"
+
+// if (try1 === "o" && try2 === "o" && try3 === "o") {
+//     alert('You Win')
+// } else {
+//     alert('You Loss')
+// }
+
+// var number1 = +prompt('Please provide number 1');
+// var number2 = +prompt('Please provide number 2');
+// var number3 = +prompt('Please provide number 3');
+
+// if (number1 >= 0 && number2 >= 0) {
+//     alert("Positive Numbers")
+// } else {
+//     alert("Negative Numbers")
+// }
+
+// 1  2  3
+
+// 3 2 1
+
+// 12  7  29
+
+// if (number1 < number2 && number2 < number3) {
+//     alert("Ascending Order")
+// } else {
+//     alert("Unordered")
+// }
+
+// && aur 
+// var favFruit = prompt("Enter your Favourite Fruit Name");
+// //  || ya
+// if (favFruit === "Banana" || favFruit === "Watermelon" || favFruit === "Guava") {
+//     alert("Available")
+// } else {
+//     alert("Not Available")
+// }
+
+var favColor = prompt("Enter Your Favourite Color");
+if (favColor === "green" || favColor === "blue" || favColor === "red") {
+    alert("Congratulation")
+} else {
+    alert("Sorry")
+}
