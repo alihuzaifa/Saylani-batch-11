e3f5bf9571d24e69e59c39778cd2b3d5bfd2cd2d
alihuzaifa <ha4587323@gmail.com>
2024-03-20 07:00:25 +0500

    tic tac toe game has been completed

--- JavaScript/9) Array/app.js
+++ JavaScript/9) Array/app.js
@@ -1,1 +1,8 @@
-var cities = ["Karachi", "Lahore", "Peshawar", "Islamabad"];
+// var cities = ["Karachi", "Lahore", "Peshawar", "Islamabad"];
+// var fruit1 = "Mango";
+// var fruit2 = "Banana";
+// var fruit3 = "Pineapple";
+
+// [] array ;
+var fruits = ["Mango", "Banana", "Apple"];
+console.log(fruits);
