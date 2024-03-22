eb9edd05b495ddb59247bb172bb11393d3a43cda
alihuzaifa <ha4587323@gmail.com>
2024-03-22 07:11:55 +0500

    basic methods of an array to perform operations on it like insert or delete with push pop shift and unshift

---
+++ JavaScript/10) Array methods/app.js
@@ -0,0 +1,10 @@
+var studentNames = ["Ali", "Aslam", "Nadir"];
+// studentNames.push("Bilal", "Asjad", "Irfan")
+// console.log(studentNames)
+// studentNames.pop()
+
+// studentNames.shift()
+// studentNames.unshift("Zubair", "Zakir")
+// console.log(studentNames)
+
+// push pop shift unshift     insert-last, delete-last, delete-first ,insert-first 
