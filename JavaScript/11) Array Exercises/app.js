f00caa8b52502ac87f119e39298bb3f08f412c61
alihuzaifa <ha4587323@gmail.com>
2024-03-25 07:55:59 +0500

    array exercises in class with slice method has been covered

---
+++ JavaScript/11) Array Exercises/app.js
@@ -0,0 +1,20 @@
+// var studentNames = ["Hazi1", "Ali", "Hamza"];
+// // var requiredArray = ["Ali", "Hamza"]
+// var requiredArray = studentNames.slice(1, 3)
+// console.log(studentNames, requiredArray);
+
+
+var studentNames = ["Haziq", "Ali", "Hamza"];
+var studentScores = [400, 420, 300];
+var index = 0;
+
+console.log("index", index)
+console.log("Score of " + studentNames[index] + " is " + studentScores[index] + ". " + (studentScores[index] / 500) * 100 + "%");
+
+index++ // 1
+console.log("index", index)
+console.log("Score of " + studentNames[index] + " is " + studentScores[index] + ". " + (studentScores[index] / 500) * 100 + "%");
+
+index++ // 2
+console.log("index", index)
+console.log("Score of " + studentNames[index] + " is " + studentScores[index] + ". " + (studentScores[index] / 500) * 100 + "%");
