5752c69d24341633b20a34e157b04714f4026a87
Meer Quais <meerquais017@gmail.com>
2024-04-24 23:00:08 +0500

    math class methods like random round ceil floor and replace method revision has covered

---
+++ JavaScript/19) Numbers/app.js
@@ -0,0 +1,54 @@
+// var text = "ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, doloribus. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, aut! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, totam! ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, doloribus. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, aut! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, totam! ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, doloribus. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, aut! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, totam! ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, doloribus. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, aut! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, totam! ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, doloribus. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, aut! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, totam! ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, doloribus. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, aut! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, totam!";
+// for (let index = 0; index < text.length; index++) {
+//     var copyText = text.slice(index, index + 5);
+//     if (copyText === "Lorem") {
+//         text = text.slice(0, index) + " Dummy " + text.slice(index + 5)
+//     }
+// }
+// var updatedText = text.replace(/Lorem/g, "Dummy");
+// console.log(updatedText)
+
+// var numbers = [12, 123, 356, 4, 5579, 909, 98, 3637, 898, 3, 76, 90009, 86238, 7];
+// var greaterNumber = 0;
+// for (var index = 0; index < numbers.length; index++) {
+//     // console.log(index)
+//     // if (index === 0) {
+//     //     greaterNumber = element
+//     // }
+//     // 4 < 12
+//     var element = numbers[index]
+//     if (element > greaterNumber) {
+//         greaterNumber = element
+//         // 0 = 12
+//         // 12 = 123
+//         // 123 = 356
+//         // 356 = 5579
+//     }
+// }
+// console.log(greaterNumber)
+// loop
+// if >
+// greater number konsa hain
+
+
+
+
+
+
+
+
+
+// var number = -1.6;
+// var roundingNumber = Math.round(number);
+// console.log(roundingNumber)
+
+
+// var number = 56.9;
+// var updatedNumber = Math.ceil(number)
+// var updatedNumber = Math.floor(number)
+
+
+
+
+var randomNumber = Math.round(Math.random())
+console.log(randomNumber)
