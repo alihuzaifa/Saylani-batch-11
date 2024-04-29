fe3555c56f856bec72ee05c72368d2dfd363635e
Meer Quais <meerquais017@gmail.com>
2024-04-29 23:00:47 +0500

    set interval and setimeout callback functions has completed and  breif about date object

---
+++ JavaScript/21) setInterval & setTimeout & Date Intro/app.js
@@ -0,0 +1,72 @@
+
+
+// function sum(val1, val2) {
+//     var answer = val1 + val2
+//     return answer
+// }
+
+// var getSum = sum(1000, 368686);
+// console.log(getSum)
+
+
+// function greet(name, callback) {
+//     var greetToUser = "Assalamu Alaikum " + name;
+//     callback(greetToUser)
+// }
+
+
+// function logMessage(response) {
+//     console.log("response", response)
+// }
+
+// greet("Nadir", logMessage)
+
+// var count = 0;
+// function greet() {
+//     console.log(count)
+//     if (count === 10) {
+//         clearInterval(timerId)
+//     } else {
+//         count++
+//     }
+// }
+// var timerId = setInterval(greet, 500)
+
+// function greet() {
+//     alert("Hello World")
+// }
+// var timerId = setTimeout(greet, 1000); //
+// clearTimeout(timerId)
+// for (let index = 0; index < 6; index++) {
+//     console.log(index)
+// }
+
+
+
+
+var currentDate = new Date()
+var currentDay = currentDate.getFullYear()
+console.log(currentDay,currentDate)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
