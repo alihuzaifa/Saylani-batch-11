87f0da03855d208f61885053ea4793f844199f82
Meer Quais <meerquais017@gmail.com>
2024-05-06 23:01:10 +0500

    date methods of get and sets with their exercises and also basic function syntax with passing data to function has covered in class

---
+++ JavaScript/23) Function/app.js
@@ -0,0 +1,95 @@
+// var today = new Date();
+// // today = today.setFullYear(2006)
+// // today = today.setMonth(3)
+// // today = today.setDate(1)
+// today = today.setHours(1)
+
+
+// var userMonth = +prompt("Enter a month number");
+// var months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// if (userMonth < 0) {
+//     alert(months[0])
+// }
+// else if (userMonth > 11) {
+//     alert(months[11])
+// }
+// else {
+//     alert(months[userMonth])
+// }
+
+
+
+
+
+
+
+
+
+
+
+// function greeting() {
+//     console.log("Hello World")
+// }
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+
+
+
+
+
+
+
+
+
+
+
+
+// function currentTime() {
+//     var currentDate = new Date();
+//     var hours = currentDate.getHours()
+//     var minutes = currentDate.getMinutes();
+//     console.log("Current Time is " + hours + " " + minutes)
+// }
+// currentTime()
+// currentTime()
+// currentTime()
+
+
+
+
+function sum(val1, val2) {
+    const sumValue = val1 + val2;
+    console.log(sumValue)
+}
+
+// Arguement jo ap function ko call karte waqt batate hain
+// Parameter jo function ko apne by default bataye hote hain
+
+sum(19000, 40000)
+sum(15, 30)
+sum(43, 79)
+sum(87, 69)
