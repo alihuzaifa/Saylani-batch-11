dff4fd81fb8947653859be724ead06ae09015f61
Meer Quais <meerquais017@gmail.com>
2024-05-08 23:08:58 +0500

    function has completed

---
+++ JavaScript/24) Function remaining Topics/app.js
@@ -0,0 +1,34 @@
+// var text = document.getElementById('special')
+
+// function calculator(val1, val2, operator) {
+//     var answer = 0;
+//     if (operator === "+") {
+//         var sum = val1 + val2;
+//         answer = sum
+//     }
+//     else if (operator === "-") {
+//         var sub = val1 - val2;
+//         answer = sub
+//     }
+//     else if (operator === "*") {
+//         var mul = val1 * val2;
+//         answer = mul
+//     }
+//     else if (operator === "/") {
+//         var div = val1 / val2;
+//         answer = div
+//     }
+//     return answer
+// }
+// var calculatorAnswer = calculator(22, 20, "+");
+// console.log(calculatorAnswer)
+// text.innerHTML = calculatorAnswer
+
+
+
+var gVariable = 10;
+function greet() {
+    var text = "Hello World";
+    console.log(text)
+}
+greet()
