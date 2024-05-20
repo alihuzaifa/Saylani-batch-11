f342322913de0646278fa94fd69e3655d180b193
alihuzaifa <ha4587323@gmail.com>
2024-05-20 22:54:04 +0500

    some events topics has been covered

---
+++ JavaScript/29)/app.js
@@ -0,0 +1,76 @@
+// // var user = document.getElementById('user-name');
+// var btn = document.getElementById('btn');
+
+
+// // function search1() {
+// //     console.log("Function 1")
+
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// btn.addEventListener("click", function () {
+//     console.log("Hello World 1")
+// })
+// btn.addEventListener("click", function () {
+//     console.log("Hello World 2")
+// })
+// btn.addEventListener("click", function () {
+//     console.log("Hello World 3")
+// })
+
+// function search2() {
+//     console.log("Function 2")
+
+// }
+
+
+
+
+
+
+
+var students = ["Ali", "Sami", "Taha", "Inam"];
+var scores = [58, 73, 89, 90];
+
+// ali 58
+// Sami 73
+
+
+
+// Ali 90
+// Sami 89
+// Sami 73
+
+
+
+
+
+
+
+
+
+
+
+
+
+
