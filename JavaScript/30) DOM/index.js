897dc99e39bfb3e374dcdbe8079e6e7bd990e5d7
alihuzaifa <ha4587323@gmail.com>
2024-05-22 23:11:31 +0500

    dom rendering html by js and adding styles by js has taught in class

---
+++ JavaScript/30) DOM/index.js
@@ -0,0 +1,42 @@
+// var btn = document.getElementById("btn");
+
+// function greet() {
+//     alert("Hello World")
+// }
+
+// // btn.addEventListener("click", greet)
+// btn.addEventListener("click", function () {
+//     alert("Hello World")
+// })
+
+
+// var list = document.getElementById("list");
+// var btn = document.getElementById("btn");
+// btn.addEventListener("click", function () {
+//     list.innerHTML = `<li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo, fugiat!</li>
+//    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo, fugiat!</li>
+//    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo, fugiat!</li>`
+// })
+
+// var number = 5;
+// number = 100;
+// console.log(number)
+
+
+
+// var heading = document.getElementById("heading");
+// var btn = document.getElementById("btn");
+// btn.addEventListener("click", function () {
+//     heading.className = heading.className + " bg"
+// })
+
+// var userYear = +prompt("Enter your birth year?");
+// var birthDate = new Date();
+// birthDate.setFullYear(userYear)
+// var today = new Date();
+
+// var convertBirthDateInMilliSecond = birthDate.getTime()
+// var convertTodayInMilliSecond = today.getTime()
+// var hours = convertTodayInMilliSecond - convertBirthDateInMilliSecond;
+
+// console.log(hours)
